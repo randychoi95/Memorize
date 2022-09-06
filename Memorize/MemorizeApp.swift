a89b9a881b98bc59a1e117ed5c5215837df82a0e
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2022-09-06 17:03:55 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 최제환 on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
