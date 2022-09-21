6413b96e0eab3c508ff34247a60a61cdfe03b12f
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2022-09-21 10:15:02 +0900

    :lipstick: CardView UI 수정
    - content size 수정
    - Grid Item size 수정

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,7 +12,7 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum:100))]) {
                 ForEach(viewModel.cards) { card in
                     CardView(card: card)
                         .aspectRatio(2/3, contentMode: .fit)
@@ -27,6 +27,43 @@ struct EmojiMemoryGameView: View {
     }
 }
 
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    shape
+                        .fill()
+                        .foregroundColor(.white)
+                    shape
+                        .strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    
+                    Text(card.content)
+                        .font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                }else {
+                    shape
+                        .fill()
+                }
+            }
+        }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        return Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
@@ -34,30 +71,5 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
         EmojiMemoryGameView(viewModel: game)
             .preferredColorScheme(.light)
-    }
-}
-
-struct CardView: View {
-    let card: MemoryGame<String>.Card
-    
-    var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                shape
-                    .fill()
-                    .foregroundColor(.white)
-                shape
-                    .strokeBorder(lineWidth: 3)
-                
-                Text(card.content)
-                    .font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            }else {
-                shape
-                    .fill()
-            }
-        }
     }
 }
