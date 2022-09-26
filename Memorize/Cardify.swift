ee8617a0d9d8e23f029f18c829fb09f4f7d95310
chlwpghks95 <chlwpghks95@awasoft.co.kr>
2022-09-26 10:34:27 +0900

    :lipstick:  Custom ViewModifier 적용
    - CardView에 viewmodifer 적용

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,40 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by 최제환 on 2022/09/22.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp { 
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                content
+            } else {
+                shape
+                    .fill()
+            }
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        return self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
